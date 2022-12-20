efa9692a19590d6262335c3c969bdd070d1043be
虫 <tokinobug@163.com>
2022-12-20 14:39:06 +0800

    add example

--- fractal_map/fractal_map.cpp
+++ fractal_map/fractal_map.cpp
@@ -9,6 +9,10 @@ fractal_map fractal_utils::fractal_map::create(size_t rows, size_t cols,
                                                size_t sizeof_element) noexcept {
   fractal_map map;
 
+  map.rows = rows;
+  map.cols = cols;
+  map.element_bytes = sizeof_element;
+
   if (rows <= 0 || cols <= 0 || sizeof_element <= 0) {
     // no memory to be allocated
     map.data = nullptr;
@@ -18,15 +22,11 @@ fractal_map fractal_utils::fractal_map::create(size_t rows, size_t cols,
 
     map.data =
 #ifdef _WIN32
-        _aligned_malloc(rows * cols * sizeof_element, 128);
+        _aligned_malloc(rows * cols * sizeof_element, 64);
 #else
-        aligned_alloc(128, rows * cols * sizeof_element);
+        aligned_alloc(64, rows * cols * sizeof_element);
 #endif
   }
-
-  map.rows = rows;
-  map.cols = cols;
-  map.element_bytes = sizeof_element;
 
   return map;
 }
---
+++ fractal_png/test_fractal_png.cpp
@@ -0,0 +1,36 @@
+#include "fractal_png.h"
+
+#include "../fractal_colors/colors.h"
+
+#include <cstring>
+#include <stdio.h>
+
+using namespace fractal_utils;
+
+int main() {
+
+  // printf("rua~\n");
+
+  fractal_map map = fractal_map::create(128, 256, 3);
+
+  // printf("address = %p\n", map.data);
+
+  memset(map.data, 0xAF, map.cols * map.rows * map.element_bytes);
+
+  bool success = true;
+
+  if (map.element_bytes == 3) {
+
+    pixel_RGB *const data = reinterpret_cast<pixel_RGB *>(map.data);
+
+    for (uint64_t i = 0; i < map.rows * map.cols; i++) {
+      data[i].value[i % 3] = 0xFF;
+    }
+
+    success = write_png("test_u8c3.png", color_space::u8c3, map);
+  }
+
+  printf("success = %i.\n", int(success));
+
+  return 0;
+}
