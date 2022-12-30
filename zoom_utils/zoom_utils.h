29bb57753ebbd1ec9577df9194996ed848e3002d
ToKiNoBug <tokinobug@163.com>
2022-12-30 19:48:08 +0800

    add zoom_utils based on Qt6

---
+++ zoom_utils/zoom_utils.h
@@ -0,0 +1,10 @@
+#ifndef FRACTALUTILS_ZOOM_UTILS_H
+#define FRACTALUTILS_ZOOM_UTILS_H
+
+#include <array>
+
+#include "core_utils.h"
+
+namespace fractal_utils {}  // namespace fractal_utils
+
+#endif  // FRACTALUTILS_ZOOM_UTILS_H
