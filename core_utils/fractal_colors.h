ce51a11c6a5e6a774f3d44edbb27df1d258bb2b9
ToKiNoBug <tokinobug@163.com>
2023-01-06 23:35:42 +0800

    new constructors for pixel_u8c3

--- core_utils/fractal_colors.h
+++ core_utils/fractal_colors.h
@@ -47,6 +47,18 @@ enum class color_series : uint8_t {
 };
 
 struct pixel_RGB {
+  inline pixel_RGB(uint8_t R, uint8_t G, uint8_t B) {
+    value[0] = R;
+    value[1] = G;
+    value[2] = B;
+  }
+
+  inline pixel_RGB(uint32_t ARGB) {
+    value[0] = (ARGB & 0x00FF0000U) >> 16;
+    value[1] = (ARGB & 0x0000FF00U) >> 8;
+    value[1] = (ARGB & 0x000000FFU);
+  }
+
   uint8_t value[3];
 };
 
@@ -62,6 +74,6 @@ void color_u8c3_many(const float *const f, const color_series,
 void color_u8c4_many(const float *const f, const color_series,
                      const size_t pixel_num, pixel_ARGB *const dest) noexcept;
 
-} // namespace fractal_utils
+}  // namespace fractal_utils
 
-#endif // FRACTALUTILS_COLORS_H
+#endif  // FRACTALUTILS_COLORS_H
