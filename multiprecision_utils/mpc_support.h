632f758f891e2d81fd3ca307c5f4467390115298
ToKiNoBug <tokinobug@163.com>
2023-06-17 19:35:20 +0800

    add mpc support

---
+++ multiprecision_utils/mpc_support.h
@@ -0,0 +1,12 @@
+//
+// Created by joseph on 6/17/23.
+//
+
+#ifndef FRACTALUTILS_MPC_SUPPORT_H
+#define FRACTALUTILS_MPC_SUPPORT_H
+
+#include <mpc.h>
+#include <boost/multiprecision/mpc.hpp>
+namespace fractal_utils {}
+
+#endif  // FRACTALUTILS_MPC_SUPPORT_H
